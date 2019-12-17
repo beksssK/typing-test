83de4f08787880e83cb181717db95e7970aacb47
Beksultan Kadyrbekov <kbeksss@gmail.com>
2019-12-18 01:25:04 +0600

    Remade the callback of the oninput event

--- js/script.js
+++ js/script.js
@@ -15,32 +15,63 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     const tempText = 'Behind sooner dining so window excuse he summer. Breakfast met certainty and fulfilled propriety led.';
-    text.innerText = tempText;
-
-    const wordsArray = tempText.split(' ');
-
-    let wordCounter = 0;
+    const lettersArray = tempText.split('');
+    let words = [];
+    for(let i = 0; i < lettersArray.length; i++){
+        words.push(`<span id=${i}>${lettersArray[i]}</span>`);
+    }
+    text.innerHTML = words.join('');
 
     writer.oninput = event => {
-        console.log(wordsArray.length);
-        if(wordCounter !== wordsArray.length){
+        if(writer.value.length <= lettersArray.length){
             if(writer.value){
-                for(let i = 0; i < writer.value.length; i++){
-                    if(wordsArray[wordCounter].substring(0, i + 1) === writer.value){
-                        showRight();
-                    } else{
-                        showWrong();
+                if(event.inputType === 'insertText'){
+                    for(let i = 0; i < writer.value.length; i++){
+                        if(event.data === lettersArray[i]){
+                            if(i > 0){
+                                let flag = document.getElementById(`${i - 1}`).className;
+                                if(flag !== 'wrong typed' && flag !== 'typed wrong'){
+                                    document.getElementById(`${i}`).classList.add('correct', 'typed');
+                                } else {
+                                    document.getElementById(`${i}`).classList.add('wrong', 'typed');
+                                }
+                            } else{
+                                document.getElementById(`${i}`).classList.add('correct', 'typed');
+                            }
+                        } else {
+                            let flag = document.getElementById(`${i}`).className;
+                            if(flag !== 'correct typed' && flag !== 'typed correct'){
+                                document.getElementById(`${i}`).classList.add('wrong', 'typed');
+                            }
+                        }
+                    }
+                } else if(event.inputType === 'deleteContentBackward'){
+                    let typed = document.getElementsByClassName('typed');
+                    let correct;
+                    let wrongId;
+                    for(let j = 0; j < typed.length; j++){
+                        let ifCorrect = typed[j].classList.value.indexOf('correct');
+                        if(ifCorrect >= 0){
+                            correct = typed[j];
+                        }
+                        let ifWrong = typed[j].classList.value.indexOf('wrong');
+                        if(ifWrong >= 0){
+                            wrongId = typed[j];
+                        }
+                    }
+                    console.log(typed);
+                    if(correct){
+                        console.log(correct.id);
+                    }
+                    if(wrongId){
+                        console.log(wrongId.id);
+                    }
+                    if(correct && wrongId){
+                        wrongId.classList.remove('wrong');
+                    } else {
+                        correct.classList.remove('correct');
                     }
                 }
-            }
-            if(event.data === ' ' && wordsArray[wordCounter] === writer.value.substring(0, writer.value.length -1)){
-                console.log('Wow');
-                writer.value = '';
-                wordCounter++;
-                console.log(wordCounter);
-            }
-            if(!writer.value){
-                removeResult();
             }
         }
     };
