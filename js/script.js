4afa48515b517bdc835aef26acae0ef36cdce77d
Beksultan Kadyrbekov <kbeksss@gmail.com>
2019-12-18 16:06:04 +0600

    Made some refactoring

--- js/script.js
+++ js/script.js
@@ -7,90 +7,19 @@ document.addEventListener('DOMContentLoaded', () => {
     const reset = document.getElementById('reset');
     const result = document.getElementById('result');
 
-
-    const tempText = 'And I am not frightened of dying, any time will do, I don\'t mind. Why should I be frightened of dying? There\'s no reason for it, you\'ve gotta go sometime';
-    const lettersArray = tempText.split('');
-    let words = [];
-    for(let i = 0; i < lettersArray.length; i++){
-        words.push(`<span id=${i}>${lettersArray[i]}</span>`);
-    }
-    text.innerHTML = words.join('');
-    let inputTimer = false;
-    writer.oninput = event => {
-        if(inputTimer){
-            if(!timerFlag){
-                timer();
-            }
-        }
-        if(writer.value.length <= lettersArray.length){
-            inputTimer = true;
-            if(event.inputType === 'insertText'){
-                for(let i = 0; i < writer.value.length; i++){
-                    if(event.data === lettersArray[i]){
-                        if(i > 0){
-                            let flag = document.getElementById(`${i - 1}`).className;
-                            if(flag !== 'wrong typed' && flag !== 'typed wrong'){
-                                document.getElementById(`${i}`).classList.add('correct', 'typed');
-                            } else {
-                                document.getElementById(`${i}`).classList.add('wrong', 'typed');
-                            }
-                        } else{
-                            document.getElementById(`${i}`).classList.add('correct', 'typed');
-                        }
-                    } else {
-                        let flag = document.getElementById(`${i}`).className;
-                        if(flag !== 'correct typed' && flag !== 'typed correct'){
-                            document.getElementById(`${i}`).classList.add('wrong', 'typed');
-                        }
-                    }
-                }
-            } else if(event.inputType === 'deleteContentBackward'){
-                let typed = document.getElementsByClassName('typed');
-                let correct;
-                let wrong;
-                for(let j = 0; j < typed.length; j++){
-                    let ifCorrect = typed[j].classList.value.indexOf('correct');
-                    if(ifCorrect >= 0){
-                        correct = typed[j];
-                    }
-                    let ifWrong = typed[j].classList.value.indexOf('wrong');
-                    if(ifWrong >= 0){
-                        wrong = typed[j];
-                    }
-                }
-
-                if(correct && wrong){
-                    wrong.classList.remove('wrong');
-                } else if(correct){
-                    correct.classList.remove('correct');
-                } else if(wrong){
-                    wrong.classList.remove('wrong');
-                }
-            }
-
-        } else{
-            inputTimer = false;
-            writer.disabled = true;
-            let words = lettersArray.length / 5;
-            let minutes = allSec / 60;
-            let wpm = words/minutes;
-            result.innerText = `Your wpm is: ${wpm}`;
-        }
-
-    };
-
     // timer
     let minutes = 0;
     let seconds = 0;
     let allSec = 0;
 
-    let toString = function(number){
+    const toString = number => {
         let string = String(number);
         while(string.length < 2){
             string = '0' + string;
         }
         return string;
     };
+
     let timerFlag = false;
     let timer = function(){
         timerFlag = true;
@@ -117,8 +46,74 @@ document.addEventListener('DOMContentLoaded', () => {
                 seconds = 0;
                 secondsE.innerText = toString(seconds);
             });
-
         }, 1000)
     };
+    // text
+    const paragraph = 'And I am not frightened of dying, any time will do, I don\'t mind. Why should I be frightened of dying? There\'s no reason for it, you\'ve gotta go sometime';
+    const lettersArray = paragraph.split('');
+    let words = [];
+    for(let i = 0; i < lettersArray.length; i++){
+        words.push(`<span id=${i}>${lettersArray[i]}</span>`);
+    }
+    text.innerHTML = words.join('');
 
+    // input
+    const addClass = (elementId, firstClass, secondClass) =>{
+        document.getElementById(elementId).classList.add(firstClass, secondClass);
+    };
+    let inputTimer = false;
+    writer.oninput = event => {
+        if(inputTimer && !timerFlag){
+            timer();
+        }
+        if(writer.value.length <= lettersArray.length){
+            inputTimer = true;
+            if(event.inputType === 'insertText'){
+                for(let i = 0; i < writer.value.length; i++){
+                    if(event.data === lettersArray[i]){
+                        if(i > 0){
+                            let previous = document.getElementById(`${i - 1}`).className;
+                            if(previous !== 'wrong typed' && previous !== 'typed wrong'){
+                                addClass(`${i}`,'correct', 'typed');
+                            } else {
+                                addClass(`${i}`, 'wrong', 'typed');
+                            }
+                        } else{
+                            addClass(`${i}`, 'correct', 'typed');
+                        }
+                    } else {
+                        let flag = document.getElementById(`${i}`).className;
+                        if(flag !== 'correct typed' && flag !== 'typed correct'){
+                            addClass(`${i}`, 'wrong', 'typed');
+                        }
+                    }
+                }
+            } else if(event.inputType === 'deleteContentBackward'){
+                let typed = document.getElementsByClassName('typed');
+                let correct;
+                let wrong;
+                for(let j = 0; j < typed.length; j++){
+                    if(typed[j].classList.value.indexOf('correct') >= 0){
+                        correct = typed[j];
+                    } else if(typed[j].classList.value.indexOf('wrong') >= 0){
+                        wrong = typed[j];
+
+                    }
+                }
+                if(correct && wrong){
+                    wrong.classList.remove('wrong');
+                } else if(correct){
+                    correct.classList.remove('correct');
+                } else if(wrong){
+                    wrong.classList.remove('wrong');
+                }
+            }
+        } else{
+            inputTimer = false;
+            writer.disabled = true;
+            let wpm = Math.round(lettersArray.length / 5) / (allSec / 60);
+            result.innerText = `Your wpm is: ${wpm}`;
+        }
+
+    };
 });
